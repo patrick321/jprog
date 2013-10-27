8d10401fb410ea73a52e77c504dd3c4543ab1d23
Philipp Schubert <pschubert@techfak.uni-bielefeld.de>
2013-10-27 09:47:19 +0100

    Patti genervt

--- test.java
+++ test.java
@@ -1,6 +1,6 @@
 public class test{
 	public static void main(String[] args){
-		System.out.println("Hallo");
+		System.out.println("Hallo Welt");
+		System.out.println("Patti du bist der GRÖßTE");
 	}
-
 }
