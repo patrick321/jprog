d019a43cfe6d1e362eeb296fd93e2544ba12b375
Patrick Schubert <Patrick.Schubert@email.de>
2013-10-27 19:16:29 +0100

    Mysql Template

--- wireshark.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get install wireshark
-sudo apt-get install libcap2-bin
-sudo groupadd wireshark
-sudo usermod -a﻿ -G wireshark $1
-sudo chgrp wireshark /usr/bin/dumpcap
-sudo chmod 755 /usr/bin/dumpcap
-sudo setcap cap_net_raw,cap_net_admin=eip /usr/bin/dumpcap
